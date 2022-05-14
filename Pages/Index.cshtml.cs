ef91290adc1a85401db6dc0dff5dae795e98d433
Carlos Durbán <kaken20@gmail.com>
2022-05-14 18:37:41 +0200

    init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcoreapp.Pages;
+
+public enum FeatureFlags
+{
+    Feature1,
+    Feature2
+}
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    
+    private readonly IConfiguration _config;
+
+
+    public IndexModel(ILogger<IndexModel> logger,IConfiguration config )
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public string Secret;
+    public void OnGet()
+    {
+            this.Secret = _config["Movies:ServiceApiKey"];
+    }
+}
